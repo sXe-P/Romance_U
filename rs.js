2e9589bf5ccd841fb6e83d13c99d8f90cabef5e7
sXe-P <69051487+sXe-P@users.noreply.github.com>
2020-08-21 01:31:47 -0400

    Add files via upload

--- rs.js
+++ rs.js
@@ -1,9 +1,27 @@
+/*navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then((stream)=>{console.log(stream) }).catch((err)=>console.log(err))*/
+
 $(document).ready(function(){
     $('#btn_radio').click(function(){
     $('#player').toggleClass('full');
     })
     
     $('#btn_fresh').click(function(){
-    $("#player1").load("https://sxe-p.github.io/Romance_U/radio.html");
+    $("#player1").load("p/player1.html");
+    })
+	
+	$('#btn-inicio').click(function(){
+    $(".box1").load("index.html");
+    })
+	
+	$('#btn-juegos').click(function(){
+    $(".box1").load("p/juegos.html");
+    })
+	
+	$('#btn-locutores').click(function(){
+    $(".box1").load("p/locutores.html");
+    })
+	
+	$('#btn-programacion').click(function(){
+    $(".box1").load("p/programacion.html");
     })
 });
