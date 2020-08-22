0a45990621f07b2c490f546f53487345a58c2435
sXe-P <69051487+sXe-P@users.noreply.github.com>
2020-08-22 15:54:05 -0400

    Add files via upload

--- rs.js
+++ rs.js
@@ -1,11 +1,13 @@
 /*navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then((stream)=>{console.log(stream) }).catch((err)=>console.log(err))*/
 
+$(document).ready(function(){
     $('#btn_radio').click(function(){
     $('#player').toggleClass('full');
     })
-
+});
+	$(".box1").load("https://romanceuniversal.top.mx/index.html");
     $('#btn_fresh').click(function(){
-    $("#player1").load("https://romanceuniversal.top.mx/p/radio.html");
+    $("#player1").load("player1.html");
     })
 
 	$('#btn-inicio').click(function(){
@@ -13,13 +15,13 @@
     })
 
 	$('#btn-juegos').click(function(){
-    $(".box1").load("https://romanceuniversal.top.mx/p/juegos.html");
+    $(".box1").load("p/juegos.html");
     })
 
 	$('#btn-locutores').click(function(){
-    $(".box1").load("https://romanceuniversal.top.mx/p/locutores.html");
+    $(".box1").load("/locutores.html");
     })
 
 	$('#btn-programacion').click(function(){
-    $(".box1").load("https://romanceuniversal.top.mx/p/programacion.html");
+    $(".box1").load("/programacion.html");
     })
