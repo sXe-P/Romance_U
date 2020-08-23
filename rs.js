53a95817901fff759ab090604c6ab0a9cfd4e106
sXe-P <69051487+sXe-P@users.noreply.github.com>
2020-08-23 02:28:14 -0400

    Add files via upload

--- rs.js
+++ rs.js
@@ -25,3 +25,41 @@ $(document).ready(function(){
 	$('#btn-programacion').click(function(){
     $(".box1").load("https://sxe-p.github.io/Romance_U/programacion.html");
     })
+
+(function(){
+		var actualizarHora = function(){
+			var fecha = new Date(),
+				horas = fecha.getHours(),
+				ampm,
+				minutos = fecha.getMinutes(),
+				segundos = fecha.getSeconds();
+			
+			var pHoras = document.getElementById('horas'),
+				pAMPM = document.getElementById('ampm'),
+				pMinutos = document.getElementById('minutos'),
+				pSegundos = document.getElementById('segundos');
+			
+			if (horas >= 12) {
+				horas = horas - 12;
+				ampm = 'PM';
+			} else {
+				ampm = 'AM';
+			}
+			
+			if (horas == 0){
+				horas = 12;
+			};
+			if (horas < 10 ){ horas = "0" + horas};
+			pHoras.textContent = horas;
+			pAMPM.textContent = ampm;
+			
+			if (minutos < 10 ){ minutos = "0" + minutos};
+			
+			pMinutos.textContent = minutos;
+			if (segundos < 10 ){ segundos = "0" + segundos};
+			pSegundos.textContent = segundos;
+		};
+		
+		actualizarHora();
+		var intervalo = setInterval(actualizarHora, 1000);
+	}())
